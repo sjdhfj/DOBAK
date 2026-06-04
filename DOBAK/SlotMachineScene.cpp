014437cfa94be0f74302d95e495f3ce58149e24c
kimyongmin519 <qwdfbns5198@gmail.com>
2026-06-04 11:01:12 +0900

    슬롯머신 렌더

--- DOBAK/SlotMachineScene.cpp
+++ DOBAK/SlotMachineScene.cpp
@@ -1,4 +1,4 @@
-#include "SlotMachineScene.h"
+﻿#include "SlotMachineScene.h"
 #include "Console.h"
 
 //int main()
@@ -6,17 +6,78 @@
 //	Init();
 //}
 
+int width = 5, height = 3;
+int** slotArr = new int* [height];
+
 void Init()
 {
-	int x = 10, y = 10;
 
-	GotoXY(x, y);
-	for (int i = 0; i < 5; ++i)
+	COORD res = GetConsoleResolution();
+	int x = res.X / 2 - 4;
+	int y = res.Y / 3 * 2;
+	const wstring ascii[] =
 	{
-		for (int j = 0; j < 10; ++j)
-		{
-			cout << "1";
-		}
-		GotoXY(x, y + i + 1);
+		L"   +-------------+   ",
+		L"   |.-----------.|   ",
+		L"   ||           ||   ",
+		L"   ||           ||   ",
+		L"   ||           ||   ",
+		L"   |+-----------+|   ",
+		L"   +-..-------..-+   ",
+		L"   .-------------.   ",
+		L"  / /===========\\ \\  ",
+		L" / /=============\\ \\ ",
+		L"/___________________\\",
+		L"\\___________________/ ",
+
+
+	};
+	int titleX = (res.X - 50) / 2;
+	int titleY = res.Y / 4;
+	SetUniCodeMode();
+	for (int i = 0; i < 12; ++i)
+	{
+		GotoXY(titleX, titleY + i);
+		wcout << ascii[i];
 	}
+	SetDefaultMode();
+
+	SetConsoleFont(L"NSimSun", { 20,25 }, FW_BOLD);
+	for (int i = 0; i < height; ++i)
+	{
+		slotArr[i] = new int[width];
+	}
+
+	RandomSlot(titleX + 6,titleY + 2);
+
+	while (true)
+	{
+		Render();
+	}
+}
+
+void Render()
+{
+	//system("cls");
+	GotoXY(0, 0);
+}
+
+void RandomSlot(int x, int y)
+{
+	for (int w = 0; w < 150; ++w)
+	{
+		GotoXY(x, y);
+		for (int i = 0; i < height; ++i)
+		{
+			for (int j = 0; j < width; ++j)
+			{
+				slotArr[i][j] = rand() % 7 + 1;
+				cout << slotArr[i][j] << " ";
+			}
+			GotoXY(x, y + i + 1);
+		}
+		Sleep(10);
+	}
+
+
 }
